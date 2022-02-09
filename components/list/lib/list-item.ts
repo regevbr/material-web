cc8169c31cd15fe04139cad93e2ebe7b326e81f1
Brandon Diamond <bdiamond@google.com>
2022-02-09 09:13:22 -0800

    bug: Use "media" attribute to identify slotted content.
    PiperOrigin-RevId: 427487895

--- components/list/lib/list-item.ts
+++ components/list/lib/list-item.ts
@@ -8,23 +8,22 @@ import {html, LitElement, PropertyValues, TemplateResult} from 'lit';
 import {property, queryAssignedElements, state} from 'lit/decorators';
 import {ClassInfo, classMap} from 'lit/directives/class-map';
 
+import {ARIARole} from '../../types/aria';
+
 /** @soyCompatible */
 export class ListItem extends LitElement {
   @property({type: String}) supportingText = '';
   @property({type: String}) trailingSupportingText = '';
 
-  @queryAssignedElements({slot: 'start', flatten: true})
-  protected startElement!: HTMLElement[];
+  @queryAssignedElements(
+      {slot: 'start', flatten: true, selector: '[media=icon]'})
+  protected leadingIcon!: HTMLElement[];
 
-  @queryAssignedElements({slot: 'end', flatten: true})
-  protected endElement!: HTMLElement[];
+  @queryAssignedElements({slot: 'end', flatten: true, selector: '[media=icon]'})
+  protected trailingIcon!: HTMLElement[];
 
   @property() hasLeadingIcon = false;
   @property() hasTrailingIcon = false;
-  @state() protected hasLeadingAvatar = false;
-  @state() protected hasLeadingThumbnail = false;
-  @state() protected hasLeadingImage = false;
-  @state() protected hasLeadingVideo = false;
 
   /** @soyTemplate */
   override render(): TemplateResult {
@@ -32,11 +31,18 @@ export class ListItem extends LitElement {
     return html`
       <li
           tabindex="0"
-          class="md3-list-item ${classMap(this.getRenderClasses())}"><!--
+          role=${this.getAriaRole()}
+          class="md3-list-item ${classMap(this.getRenderClasses())}"
+          @click=${this.handleClick}><!--
         -->${this.renderStart()}<!--
         -->${this.renderBody()}<!--
         -->${this.renderEnd()}<!--
       --></li>`;
+  }
+
+  /** @soyTemplate */
+  protected getAriaRole(): ARIARole {
+    return 'listitem';
   }
 
   /** @soyTemplate */
@@ -45,10 +51,6 @@ export class ListItem extends LitElement {
       'md3-list-item--with-one-line': this.supportingText === '',
       'md3-list-item--with-two-lines': this.supportingText !== '',
       'md3-list-item--with-leading-icon': this.hasLeadingIcon,
-      'md3-list-item--with-leading-avatar': this.hasLeadingAvatar,
-      'md3-list-item--with-leading-thumbnail': this.hasLeadingThumbnail,
-      'md3-list-item--with-leading-image': this.hasLeadingImage,
-      'md3-list-item--with-leading-video': this.hasLeadingVideo,
       'md3-list-item--with-trailing-icon': this.hasTrailingIcon,
     };
   }
@@ -101,22 +103,18 @@ export class ListItem extends LitElement {
   }
 
   override update(changedProperties: PropertyValues<this>) {
-    this.updateItemContext();
+    this.updateMetadata();
     super.update(changedProperties);
   }
 
-  private updateItemContext() {
-    this.hasLeadingIcon = this.startElement.some(
-        (el) => el.classList.contains('md3-list-item__icon'));
-    this.hasTrailingIcon = this.endElement.some(
-        (el) => el.classList.contains('md3-list-item__icon'));
-    this.hasLeadingAvatar = this.startElement.some(
-        (el) => el.classList.contains('md3-list-item__avatar'));
-    this.hasLeadingThumbnail = this.startElement.some(
-        (el) => el.classList.contains('md3-list-item__thumbnail'));
-    this.hasLeadingImage = this.startElement.some(
-        (el) => el.classList.contains('md3-list-item__image'));
-    this.hasLeadingVideo = this.startElement.some(
-        (el) => el.classList.contains('md3-list-item__video'));
+  private updateMetadata() {
+    this.hasLeadingIcon = this.leadingIcon.length > 0;
+    this.hasTrailingIcon = this.trailingIcon.length > 0;
+  }
+
+  protected handleClick() {
+    this.dispatchEvent(new CustomEvent(
+        'list-item-interaction',
+        {detail: {state: {selected: false}}, bubbles: true, composed: true}));
   }
 }
