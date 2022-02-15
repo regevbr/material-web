436bc7af9396134ab8bbde5d0802c15a0fa4919d
Brandon Diamond <bdiamond@google.com>
2022-02-15 10:51:08 -0800

    feat: List controller should manage Item events.
    PiperOrigin-RevId: 428823220

--- components/list/lib/list.ts
+++ components/list/lib/list.ts
@@ -6,17 +6,36 @@
 
 import {html, LitElement, TemplateResult} from 'lit';
 
+import {ARIARole} from '../../types/aria';
+
+import {ListItemInteractionEvent} from './constants';
+
 /** @soyCompatible */
 export class List extends LitElement {
   static override shadowRootOptions:
       ShadowRootInit = {mode: 'open', delegatesFocus: true};
 
   /** @soyTemplate */
+  protected getAriaRole(): ARIARole {
+    return 'list';
+  }
+
+  /** @soyTemplate */
   override render(): TemplateResult {
     return html`
-      <ul tabindex="0" class="md3-list">
+      <ul class="md3-list"
+          tabindex="0"
+          role=${this.getAriaRole()}
+          @list-item-interaction=${this.handleItemInteraction}>
         <slot></slot>
       </ul>
     `;
+  }
+
+
+  handleItemInteraction(event: ListItemInteractionEvent) {
+    if (event.detail.state.isSelected) {
+      // TODO: manage selection state.
+    }
   }
 }
---
+++ components/list/lib/state.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * The state of a list item.
+ */
+export interface ListItemState {
+  /**
+   * Index of the active navigation tab.
+   */
+  isSelected: boolean;
+}
